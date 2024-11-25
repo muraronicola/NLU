7245413a80774f0af806bbf8cc6ae12aae54414c
muraronicola <nicola.muraro@studenti.unitn.it>
2024-11-25 11:48:53 +0100

    structure first report done

--- 248449_nicola_muraro/LM/_tmp_report/plot/makeplot_loss.py
+++ 248449_nicola_muraro/LM/_tmp_report/plot/makeplot_loss.py
@@ -4,15 +4,15 @@ import pandas as pd
 
 
 if __name__ == '__main__':
-    indice_rete = 1
+    indice_rete = 2
     data = pd.read_csv('./data/network_' + str(indice_rete) + '.csv')
     fig1 = plt.figure(figsize=(7, 4))
     ax = fig1.add_subplot(111)
-    #fig1.suptitle('Network ' + str(indice_rete), fontsize=16)
-    sns.lineplot(x='epoch', y='train_ppl', data=data, ax=ax, label='Train')
-    sns.lineplot(x='epoch', y='dev_ppl', data=data, ax=ax, label='Dev')
+    #fig1.suptitle('Loss values', fontsize=14)
+    sns.lineplot(x='epoch', y='train_loss', data=data, ax=ax, label='Train')
+    sns.lineplot(x='epoch', y='dev_loss', data=data, ax=ax, label='Dev')
     ax.legend(fontsize=12)
     ax.set_xlabel('Epoch', fontsize=12)
-    ax.set_ylabel('PPL', fontsize=12)
+    ax.set_ylabel('Loss', fontsize=12)
     fig1.tight_layout()
-    plt.savefig('./results/plot_' + str(indice_rete) + '.png')
+    plt.savefig('./results/plot_' + str(indice_rete) + '_loss.png')
