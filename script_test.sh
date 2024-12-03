dba4141c9936663f329b12ac970c7fe6882473ec
muraronicola <nicola.muraro@studenti.unitn.it>
2024-12-03 11:57:04 +0100

    improoved tests

--- script_test.sh
+++ script_test.sh
@@ -1,4 +1,5 @@
 cd ./248449_nicola_muraro/LM/part_1/
+echo "LM part 1"
 
 time python3 ./main.py -e=True --model=./bin/best_model.pt
 time python3 ./main.py -s=True 
@@ -6,7 +7,7 @@ time python3 ./main.py -e=True --model=./bin/best_model_0.pt
 
 
 cd ../part_2/
-
+echo "LM part 2"
 time python3 ./main.py -e=True --model=./bin/best_model.pt
 time python3 ./main.py -s=True 
 time python3 ./main.py -e=True --model=./bin/best_model_0.pt
@@ -17,14 +18,14 @@ time python3 ./main.py -e=True --model=./bin/best_model_0.pt
 
 
 cd ../../NLU/part1/
-
+echo "NLU part 1"
 time python3 ./main.py -e=True --model=./bin/best_model.pt
 time python3 ./main.py -s=True 
 time python3 ./main.py -e=True --model=./bin/best_model_0.pt
 
 
 cd ../part_2/
-
+echo "NLU part 2"
 time python3 ./main.py -e=True --model=./bin/best_model.pt
 time python3 ./main.py -s=True 
 time python3 ./main.py -e=True --model=./bin/best_model_0.pt
@@ -37,7 +38,7 @@ time python3 ./main.py -e=True --model=./bin/best_model_0.pt
 
 
 cd ../../SA/part1/
-
+echo "SA part 1"
 time python3 ./main.py -e=True --model=./bin/best_model.pt
 time python3 ./main.py -s=True 
 time python3 ./main.py -e=True --model=./bin/best_model_0.pt
